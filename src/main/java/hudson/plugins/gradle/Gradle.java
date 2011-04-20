5d1fbe5047c247148f15a64cff90acbfa14b93b8
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2011-04-20 22:13:50 +0200

    First cut on using the Tooling API in the Gradle plugin

--- src/main/java/hudson/plugins/gradle/Gradle.java
+++ src/main/java/hudson/plugins/gradle/Gradle.java
@@ -1,19 +1,34 @@
 package hudson.plugins.gradle;
 
-import hudson.*;
-import hudson.model.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import hudson.CopyOnWrite;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.model.Computer;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.tools.ToolInstallation;
-import hudson.util.ArgumentListBuilder;
 import net.sf.json.JSONObject;
-import org.apache.commons.lang.StringUtils;
+import org.gradle.tooling.BuildLauncher;
+import org.gradle.tooling.GradleConnectionException;
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.ResultHandler;
+import org.gradle.tooling.model.eclipse.EclipseProject;
+import org.gradle.util.UncheckedException;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
 
 
 /**
@@ -53,8 +68,6 @@ public class Gradle extends Builder {
      * Flag whether to use the gradle wrapper rather than a standard Gradle installation
      */
     private final boolean useWrapper;
-
-    private static final String JAVA_OPTS_KEY = "JAVA_OPTS";
 
     @DataBoundConstructor
     public Gradle(String description, String switches, String tasks, String rootBuildScriptDir, String buildFile,
@@ -97,120 +110,93 @@ public class Gradle extends Builder {
         return rootBuildScriptDir;
     }
 
-    /**
-     * Gets the GradleBuilder to invoke, or null to invoke the default one.
-     */
-    public GradleInstallation getGradle() {
-        for (GradleInstallation i : getDescriptor().getInstallations()) {
-            if (gradleName != null && i.getName().equals(gradleName)) {
-                return i;
-            }
-        }
-        return null;
-    }
-
     @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+    public boolean perform(AbstractBuild<?, ?> build, Launcher l, final BuildListener listener)
             throws InterruptedException, IOException {
+        GradleConnector connector = GradleConnector.newConnector();
+        ProjectConnection connection =
+                connector.
+                    forProjectDirectory(projectDir(build)).
+                    useInstallation(gradleInstallationDir(build, listener)).
+                        connect();
 
-        EnvVars env = build.getEnvironment(listener);
-
-        String extraSwitches = env.get("GRADLE_EXT_SWITCHES");
-        String normalizedSwitches;
-        if (extraSwitches != null) {
-            normalizedSwitches = switches + " " + extraSwitches;
-        } else {
-            normalizedSwitches = switches;
-        }
-        normalizedSwitches = normalizedSwitches.replaceAll("[\t\r\n]+", " ");
-        normalizedSwitches = Util.replaceMacro(normalizedSwitches, env);
-        normalizedSwitches = Util.replaceMacro(normalizedSwitches, build.getBuildVariables());
-
-
-        String extraTasks = env.get("GRADLE_EXT_TASKS");
-        String normalizedTasks;
-        if (extraTasks != null) {
-            normalizedTasks = tasks + " " + extraTasks;
-        } else {
-            normalizedTasks = tasks;
-        }
-        normalizedTasks = normalizedTasks.replaceAll("[\t\r\n]+", " ");
-        ArgumentListBuilder args = new ArgumentListBuilder();
-        GradleInstallation ai = getGradle();
-        if (ai == null) {
-            if (useWrapper) {
-                String execName = (Functions.isWindows())?GradleInstallation.WINDOWS_GRADLE_WRAPPER_COMMAND:GradleInstallation.UNIX_GRADLE_WRAPPER_COMMAND;
-                FilePath workspace = build.getModuleRoot();
-                File gradleWrapperFile = new File(workspace.getRemote(), execName);
-                args.add(gradleWrapperFile.getAbsolutePath());
-            } else {
-                args.add(launcher.isUnix() ? GradleInstallation.UNIX_GRADLE_COMMAND : GradleInstallation.WINDOWS_GRADLE_COMMAND);
-            }
-        } else {
-            ai = ai.forNode(Computer.currentComputer().getNode(), listener);
-            ai = ai.forEnvironment(env);
-            String exe;
-            if (useWrapper) {
-                exe = ai.getWrapperExecutable(launcher, build);
-            } else {
-                exe = ai.getExecutable(launcher);
-            }
-            if (exe == null) {
-                listener.fatalError("ERROR");
-                return false;
-            }
-            args.add(exe);
-        }
-        args.addKeyValuePairs("-D", build.getBuildVariables());
-        args.addTokenized(normalizedSwitches);
-        args.addTokenized(normalizedTasks);
-        if (buildFile != null && buildFile.trim().length() != 0) {
-            String buildFileNormalized = Util.replaceMacro(buildFile.trim(), env);
-            args.add("-b");
-            args.add(buildFileNormalized);
-        }
-        if (ai != null) {
-            env.put("GRADLE_HOME", ai.getHome());
+        final EclipseProject model = connection.getModel(EclipseProject.class);
+        for (EclipseProject child : model.getChildren()){
+            System.out.println("child.getSourceDirectories() = " + new ArrayList(child.getSourceDirectories().getAll()));
+            System.out.println("child.getProjectDependencies() = " + new ArrayList(child.getProjectDependencies().getAll()));
         }
 
-        if (!launcher.isUnix()) {
-            // on Windows, executing batch file can't return the correct error code,
-            // so we need to wrap it into cmd.exe.
-            // double %% is needed because we want ERRORLEVEL to be expanded after
-            // batch file executed, not before. This alone shows how broken Windows is...
-            args.prepend("cmd.exe", "/C");
-            args.add("&&", "exit", "%%ERRORLEVEL%%");
-        }
+        BuildLauncher launcher = connection.newBuild();
+        final PrintStream logger = listener.getLogger();
+        final BlockingResultHandler<Void> handler = new BlockingResultHandler<Void>(Void.class);
 
-        FilePath rootLauncher;
-        if (rootBuildScriptDir != null && rootBuildScriptDir.trim().length() != 0) {
-            String rootBuildScriptNormalized = Util.replaceMacro(rootBuildScriptDir.trim(), env);
-            rootBuildScriptNormalized = Util.replaceMacro(rootBuildScriptNormalized, build.getBuildVariableResolver());
-            rootLauncher = new FilePath(build.getModuleRoot(), rootBuildScriptNormalized);
-        } else {
-            rootLauncher = build.getModuleRoot();
-        }
-        String javaOpts = env.get(JAVA_OPTS_KEY);
-        if (StringUtils.isNotBlank(javaOpts)) {
-            env.remove(JAVA_OPTS_KEY);
-        }
-        try {
-            int r = launcher.launch().cmds(args).envs(env).stdout(listener).pwd(rootLauncher).join();
-            boolean success = r == 0;
-            // if the build is successful then set it as success otherwise as a failure.
-            build.setResult(Result.SUCCESS);
-            if (!success) {
-                build.setResult(Result.FAILURE);
-            }
-            return success;
-        } catch (IOException e) {
-            Util.displayIOException(e, listener);
-            e.printStackTrace(listener.fatalError("command execution failed"));
-            build.setResult(Result.FAILURE);
-            return false;
-        }
+        launcher.forTasks(tasks.split("[ ,]")).
+                setStandardOutput(logger).setStandardError(logger).run(handler);
+
+        handler.getResult();
+        connection.close();
+
+        return true;
     }
 
+    private File gradleInstallationDir(final AbstractBuild<?, ?> build, final BuildListener listener) throws IOException, InterruptedException {
+        GradleInstallation gradleInstallation = null;
+
+        for (GradleInstallation gi : getDescriptor().getInstallations()) {
+            if (gradleName != null && gi.getName().equals(gradleName)) {
+                gradleInstallation = gi;
+            }
+        }
+
+        if (gradleInstallation == null) {
+            throw new IllegalStateException("No Gradle installation selected");
+        }
+
+        gradleInstallation = gradleInstallation.forEnvironment(build.getEnvironment(listener));
+        gradleInstallation = gradleInstallation.forNode(Computer.currentComputer().getNode(), listener);
+
+        return new File(gradleInstallation.getHome());
+    }
+
+    private File projectDir(final AbstractBuild<?, ?> build) {
+        FilePath workspace = build.getModuleRoot();
+        return new File(workspace.getRemote());
+    }
+
+    public static class BlockingResultHandler<T> implements ResultHandler<T> {
+        private final BlockingQueue<Object> queue = new ArrayBlockingQueue<Object>(1);
+        private final Class<T> resultType;
+        private static final Object NULL = new Object();
+
+        public BlockingResultHandler(Class<T> resultType) {
+            this.resultType = resultType;
+        }
+
+        public T getResult() {
+            Object result;
+            try {
+                result = queue.take();
+            } catch (InterruptedException e) {
+                throw UncheckedException.asUncheckedException(e);
+            }
+
+            if (result instanceof Throwable) {
+                throw UncheckedException.asUncheckedException((Throwable) result);
+            }
+            if (result == NULL) {
+                return null;
+            }
+            return resultType.cast(result);
+        }
+
+        public void onComplete(T result) {
+            queue.add(result == null ? NULL : result);
+        }
+
+        public void onFailure(GradleConnectionException failure) {
+            queue.add(failure);
+        }
+    }
 
     @Override
     public DescriptorImpl getDescriptor() {
@@ -273,5 +259,4 @@ public class Gradle extends Builder {
             return (Gradle) request.bindJSON(clazz, formData);
         }
     }
-
 }
