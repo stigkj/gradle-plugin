6ac8ef9ca7068e8dc455a3b2737b443a54e7433f
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2011-04-23 00:20:15 +0200

    No need for XSTREAM aliases as this is a new plugin without any old "luggage"

--- src/main/java/hudson/plugins/maven/PluginImpl.java
+++ src/main/java/hudson/plugins/maven/PluginImpl.java
@@ -24,7 +24,6 @@
 package hudson.plugins.maven;
 
 import hudson.Plugin;
-import hudson.model.Items;
 
 /**
  * @author huybrechts
@@ -34,10 +33,5 @@ public class PluginImpl extends Plugin {
     public void start() throws Exception {
         super.start();
 
-        Items.XSTREAM.alias("maven2", MavenModule.class);
-        Items.XSTREAM.alias("dependency", ModuleDependency.class);
-        Items.XSTREAM.alias("maven2-module-set", MavenModule.class);  // this was a bug, but now we need to keep it for compatibility
-        Items.XSTREAM.alias("maven2-moduleset", MavenModuleSet.class);
     }
-
 }
